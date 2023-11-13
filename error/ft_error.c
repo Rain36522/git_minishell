2a9f102ef779320fd17d520f7546a5b6b1f7de6a
Cecile Duffaut <cduffaut@c1r14s5.42lausanne.ch>
2023-11-13 11:15:48 +0100

    Auto push

--- error/ft_error.c
+++ error/ft_error.c
@@ -6,33 +6,33 @@
 /*   By: cduffaut <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 10:52:09 by cduffaut          #+#    #+#             */
-/*   Updated: 2023/11/13 11:10:39 by cduffaut         ###   ########.fr       */
+/*   Updated: 2023/11/13 11:15:26 by cduffaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Includes/minishell.h"\
+#include "Includes/minishell.h"
 
-int		ft_error(int code);
+int	ft_error(int code)
 {
 	if (code == 1)
 		ft_error_msg("command not found\n");
-	if (code == 10)
+	else if (code == 10)
 		ft_error_msg("username not found\n");
-	if (code == 11)
+	else if (code == 11)
 		ft_error_msg("error cwd\n");
-	if (code == 101)
+	else if (code == 101)
 		ft_error_msg("memory allocation failed\n");
 	if (code >= 100)
 		ft_quit();
 	return (0);
 }
 
-void	ft_error_msg(char *msg);
+void	ft_error_msg(char *msg)
 {
 	ft_printf("%s", msg);
 }
 
-int		ft_quit(void)
+int	ft_quit(void)
 {
 	// command pour tout free
 	exit(1);
