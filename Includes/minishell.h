0a7e500a87f67f1c7ecdbcddb836e1a5e9992309
Paul Cyrille Udry <pudry@c1r13s4.42lausanne.ch>
2023-11-13 16:50:11 +0100

    Auto push

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pudry <pudry@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 10:21:32 by pudry             #+#    #+#             */
-/*   Updated: 2023/11/13 14:23:33 by pudry            ###   ########.fr       */
+/*   Updated: 2023/11/13 16:47:20 by pudry            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,21 @@ char	*ft_error_str(int icode);
 char	*ft_error_int(int icode);
 int		check_up_down_key(char *str);
 char	*get_readline(int fd);
+void	ft_replace_line(char *prompt, char *s2);
+
+// typedef struct s_history
+// {
+// 	t_history	*next;
+// 	t_history	*last;
+// 	char	*cmd;
+// }	t_history;
+
+// typedef struct s_data
+// {
+// 	t_history	*hist_start;
+
+// }	t_data;
+
+
 
 #endif
--- utils/ft_utils.c
+++ utils/ft_utils.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pudry <pudry@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/13 08:52:43 by pudry             #+#    #+#             */
-/*   Updated: 2023/11/13 16:49:26 by pudry            ###   ########.fr       */
+/*   Created: 2023/11/13 16:44:46 by pudry             #+#    #+#             */
+/*   Updated: 2023/11/13 16:47:00 by pudry            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Includes/minishell.h"
-#include <readline/history.h>
+#include "../Includes/minishell.h"
 
-int	main(int argc, char ** argv, char **env)
+void	ft_replace_line(char *prompt, char *s2)
 {
-	int		i = 0;
-	char	*s;
-	char	**array;
-
-	while (i == 0)
-	{
-		i ++;
-		s = get_readline(1);
-		ft_printf("salut");
-		sleep(2);
-		// if (check_up_down_key(s) != 0)
-		// 	ft_history();
-
-    	// Efface la ligne
-		ft_replace_line("pudry@pwd : ", "bonjour\n");
-	}
+	ft_printf("\r");
+	ft_printf("\033[2K");
+	ft_printf("%s%s", prompt, s2);
 }
