a1ac9e78dbe347bf7e1b00bc84ebd28d289036c8
Cecile Duffaut <cduffaut@c1r14s5.42lausanne.ch>
2023-11-13 11:11:17 +0100

    add error file

---
+++ error/ft_error.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cduffaut <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 10:52:09 by cduffaut          #+#    #+#             */
+/*   Updated: 2023/11/13 11:10:39 by cduffaut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Includes/minishell.h"\
+
+int		ft_error(int code);
+{
+	if (code == 1)
+		ft_error_msg("command not found\n");
+	if (code == 10)
+		ft_error_msg("username not found\n");
+	if (code == 11)
+		ft_error_msg("error cwd\n");
+	if (code == 101)
+		ft_error_msg("memory allocation failed\n");
+	if (code >= 100)
+		ft_quit();
+	return (0);
+}
+
+void	ft_error_msg(char *msg);
+{
+	ft_printf("%s", msg);
+}
+
+int		ft_quit(void)
+{
+	// command pour tout free
+	exit(1);
+}
