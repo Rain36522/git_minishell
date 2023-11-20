be14bba8d341f3634650cabc45548e18b0effd3f
Paul Cyrille Udry <pudry@c1r13s6.42lausanne.ch>
2023-11-20 17:05:09 +0100

    Auto push

--- get_cmd/get_cmd.c
+++ get_cmd/get_cmd.c
@@ -6,7 +6,7 @@
 /*   By: pudry <pudry@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 10:48:32 by pudry             #+#    #+#             */
-/*   Updated: 2023/11/20 16:38:58 by pudry            ###   ########.fr       */
+/*   Updated: 2023/11/20 17:01:40 by pudry            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,28 @@ static int	ft_check_dbl_redi(char **array)
 	return (0);
 }
 
+static int	ft_check_dbl_redi_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str)
+	{
+		if (i == 0 && *str == '\"')
+			i = 2;
+		else if (i == 2 && *str == '\"')
+			i = 0;
+		else if (i == 0 && *str == '\'')
+			i = 1;
+		else if (i == 1 && *str == '\'')
+			i = 0;
+		else if (i == 0 && str[0] == '<' && str[1] == '<' && str[2] != '<')
+			return (1);
+		*str ++;
+	}
+	return (0);
+}
+
 void	ft_put_array(char **a)
 {
 	int	i;
@@ -75,11 +97,14 @@ char	**ft_make_cmd_quote(char *scmd)
 	char	*ptr2;
 	char	acmd;
 
-	if (ft_quotes)
+	if (ft_quotes(scmd, 0) == 0)
+		return (ft_split_minishell(scmd));
+	else if (ft_check_dbl_redi_str(scmd))
+		ft_error_str(1000);
+	i = ft_quotes(scmd, 0);
 	while (i != 0 && scmd)
 	{
 		scmd = ft_strjoin(scmd, "\n");
-		ft_printf("%s", scmd);
 		ptr = scmd;
 		ptr2 = readline("> ");
 		i = ft_quotes(ptr2, i);
@@ -87,9 +112,9 @@ char	**ft_make_cmd_quote(char *scmd)
 		free(ptr);
 		free(ptr2);
 	}
-}
 	if (!scmd)
 		return (NULL);
+	return (ft_split_minishell(scmd));
 }
 
 char	**get_cmd(char *prompt)
@@ -102,12 +127,11 @@ char	**get_cmd(char *prompt)
 	if (!scmd)
 		return (NULL);
 	add_history(scmd);
-	array = ft_split_minishell(scmd);
-	if (!array)
-		return (NULL);
-	if (ft_check_dbl_redi(array) == 1 && ft_quotes(scmd, 0) == 0)
+	if (ft_check_dbl_redi_str(scmd) == 1 && ft_quotes(scmd, 0) == 0)
 	{
-		array = from_quotes_to_wrds(array);
+		array = ft_split_minishell(scmd);
+		if (!array)
+			return (NULL);
 		free(scmd);
 		scmd = NULL;
 		return (ft_make_dbl_redir(array));
