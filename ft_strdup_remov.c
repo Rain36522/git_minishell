6af57a1e708abd16972a22eefb669d3c9e30fa06
Cecile Duffaut <cduffaut@c2r5s3.42lausanne.ch>
2023-11-24 14:52:20 +0100

    ji poush

---
+++ ft_strdup_remov.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup_remov.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cduffaut <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 15:51:41 by cduffaut          #+#    #+#             */
+/*   Updated: 2023/11/23 16:05:57 by cduffaut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Includes/minishell.h"
+#include <stdio.h>
+
+// attention return error malloc
+char	*ft_strdup_remov(char *str)
+{
+	int		last;
+	int		i;
+	char	*dup;
+
+	last = ft_strlen(str);
+	dup = NULL;
+	if (str[last - 1] == '\n')
+	{
+		dup = malloc(sizeof(char) * last);
+		if  (!dup)
+			return (NULL);
+		i = 0;
+		while (str[i + 1])
+		{
+			dup[i] = str[i];
+			i++;
+		}
+		dup[i] = '\0';
+		free(str);
+		return (dup);
+	}
+	return (str);
+}
+
+/*int		main(void)
+{
+	printf ("%s", ft_strdup_remov("Bijour\n\n\n\n"));
+	return (0);
+}*/
