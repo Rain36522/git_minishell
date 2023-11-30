0f9665318280e9cb1d453679030b20e4bf34e45a
Cecile Duffaut <cduffaut@c1r14s5.42lausanne.ch>
2023-11-30 16:25:53 +0100

    Auto push

--- builtin_checker.c
+++ builtin_checker.c
@@ -11,19 +11,48 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include "Includes/header_builtin.h"
+#include "Includes/minishell.h"
 
-void	builtin_checker(char *str, char **tab_cmd)
+// If that goes in one of those conditions, it should not
+// "return" to the callong child process
+// It should stop
+
+void	builtin_checker_2(char *str, char **envp)
 {
-	if (ft_strncmp(str, "echo", 5) == 0)
-		echo_cmd(tab_cmd);
+	if (ft_strncmp(str, "export", 7) == 0)
+	{
+		export_cmd(envp, str);
+		exit;
+	}
+	//else if (ft_strncmp(str, "unset", 8) == 0)
+	//	unset_cmd(str);
+	//else if (ft_strncmp(str, "exit", 5) == 0)
+	//	exit_cmd(str);
+}
+
+void	builtin_checker(t_pipex *list, char *str, char **tab_cmd, char **envp)
+{
+	char	**tmp;
+
+	tmp = NULL;
+	if (!str)
+		ft_error_int(127, 1, NULL, NULL);
+	else if (ft_strncmp(str, "echo", 5) == 0)
+	{
+		echo_cmd(str, envp);
+		exit;
+	}
 	else if (ft_strncmp(str, "cd", 3) == 0)
-		cd_cmd(tab_cmd);
+	{
+		init_cmd(str, envp);
+		exit;
+	}
 	else if (ft_strncmp(str, "env", 4) == 0)
-		env_cmd(tab_cmd);
-	else if (ft_strncmp(str, "export", 7) == 0)
-		export_cmd(tab_cmd);
-	else if (ft_strncmp(str, "unset", 8) == 0)
-		unset_cmd(tab_cmd);
-	else if (ft_strncmp(str, "exit", 5) == 0)
-		exit_cmd(tab_cmd);
+	{
+		env_cmd(str, envp);
+		exit;
+	}
+	else
+		builtin_checker_2(str, envp);
 }
