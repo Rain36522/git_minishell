fa99e982a8b6a3057116eb69f5b6200e0af6e22f
Paul Cyrille Udry <pudry@c2r6s4.42lausanne.ch>
2023-12-07 14:47:17 +0100

    Auto push

--- builtin/cwd.c
+++ builtin/cwd.c
@@ -6,7 +6,7 @@
 /*   By: pudry <pudry@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 10:18:21 by pudry             #+#    #+#             */
-/*   Updated: 2023/12/07 14:34:56 by pudry            ###   ########.fr       */
+/*   Updated: 2023/12/07 14:37:57 by pudry            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ char	*cwd(char **env)
 {
 	char	str[1024];
 	char	*ptr;
-	printf("error before\n");
+
 	ptr = getcwd(str, 1024);
 	if (!ptr)
 		return (ft_error_ptr(12, 1, env, NULL));
-	printf("error after: %s\n", ptr);
 	ptr = ft_strdup(str);
 	return (ptr);
 }
